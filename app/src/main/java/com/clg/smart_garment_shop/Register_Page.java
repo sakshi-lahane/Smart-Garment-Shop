23962dc032b515e65840203aa87e48390d5c9190
Administrator <lahanesakshi952@gmail.com>
2025-12-19 14:42:50 +0530

    feat: Add login, register pages and enhance welcome screen
    This commit introduces new, empty activities for user login and registration. The welcome page UI has been significantly updated with a new layout, imagery, and calls to action.
    
    Changes include:
    - Creating `Login_Page` and `Register_Page` activities with their basic layout files.
    - Registering the new activities in `AndroidManifest.xml`.
    - Overhauling `activity_welcome_page.xml` to use a `LinearLayout` with an image, titles, and buttons for "Get Started" and "Login".
    - Adding a new `welcome_clothes.png` drawable for the updated welcome screen.
    - Updating the `ViewCompat` listener in `Welcome_Page.java` to reference the new main `ImageView`.

--- app/src/main/java/com/clg/smart_garment_shop/Register_Page.java
+++ app/src/main/java/com/clg/smart_garment_shop/Register_Page.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Welcome_Page extends AppCompatActivity {
+public class Register_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.imgWelcome), (v, insets) -> {
+        setContentView(R.layout.activity_register_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
